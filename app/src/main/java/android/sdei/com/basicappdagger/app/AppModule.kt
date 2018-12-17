0b75edf4b3e3aa61be66e626b2fd11da3d18a82a
neeraj moudgil <neeraj.moudgil@smartdatainc.net>
2018-12-17 12:38:51 +0530

    added deteket check for kotlin

--- app/src/main/java/android/sdei/com/basicappdagger/app/AppModule.kt
+++ app/src/main/java/android/sdei/com/basicappdagger/app/AppModule.kt
@@ -10,7 +10,7 @@ import dagger.Provides
 import javax.inject.Singleton
 
 @Module
-class AppModule constructor(application: Application) {
+class AppModule (application: Application) {
     private var context: Context?=null
     val PREF_NAME = "InviteGreenPref"
     private val PRIVATE_MODE = 0
